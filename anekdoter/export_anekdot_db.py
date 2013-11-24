56f29dff5c822fbdbb8599045c1e31fe4da9e92b
Chernousov Anton <acisi82@mail.ru>
2013-11-24 21:16:18 +0700

    Freez project

---
+++ anekdoter/export_anekdot_db.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import psycopg2
+conn_string = "host='127.0.0.1' dbname='portal' user='portal' password='killa11'"
+conn=psycopg2.connect(conn_string)
+curr=conn.cursor()
+curr.execute('SELECT text FROM acisi_anekdot')
+anekdots=curr.fetchall()
+f = open('./anekdot_list.txt','w')
+for anekdot in anekdots:
+	print (anekdot[0])
+	f.write(anekdot[0])
+	print ("=============================================\n")
+	f.write('==[acisi]==')
+f.close()
+curr.close()
+conn.close()
